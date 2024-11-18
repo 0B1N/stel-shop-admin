00f126ce7eadde346c3b6d94f5b65e10ff566a12
김영빈 <youngbin@gim-yeongbins-MacBook-Pro.local>
2024-11-18 14:53:01 +0900

    config 수정 , 페이지 구조 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
 };
 
 export default nextConfig;
